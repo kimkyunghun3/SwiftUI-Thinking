a90eee2fba3aa95cffec3f085a42bae260378f31
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-04-17 08:36:46 +0900

    first commit

---
+++ SwiftUIThinking/SwiftUIThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingApp.swift
+//  SwiftUIThinking
+//
+//  Created by Eddy on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
