c5de6891e4f0cc3c50fd9f82e4893a0ba1fd65d6
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-05-07 15:25:01 +0900

    feat: Spring 관련 애니메이션 실험 추가

--- SwiftUIThinking/AnimationTimingBootcamp.swift
+++ SwiftUIThinking/AnimationTimingBootcamp.swift
@@ -18,13 +18,94 @@ struct AnimationTimingBootcamp: View {
                 isAnimating.toggle()
             }
 
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: isAnimating ? 350: 50, height: 100)
-                .animation(.spring(
-                    response: 0.5,
-                    dampingFraction: 0.7,
-                    blendDuration: 1.0
-                ))
+            // MARK: - response 테스트
+
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    // 몇초에 걸쳐서 작동할지 결정하는 부분
+//                    response: 0.5,
+//                    dampingFraction: 0.7,
+//                    blendDuration: 1.0
+//                ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    response: 1.0,
+//                    dampingFraction: 0.7,
+//                    blendDuration: 1.0
+//                ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    response: 0.0,
+//                    dampingFraction: 0.7,
+//                    blendDuration: 1.0
+//                ))
+
+            // MARK: - dampingFraction 테스트
+
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    response: 0.5,
+//                    dampingFraction: 0.0,
+//                    blendDuration: 1.0
+//                ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    response: 0.5,
+//                    dampingFraction: 0.5,
+//                    blendDuration: 1.0
+//                ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350: 50, height: 100)
+//                .animation(.spring(
+//                    response: 0.5,
+//                    dampingFraction: 1.0,
+//                    blendDuration: 1.0
+//                ))
+
+            // MARK: - blendDuration 테스트
+
+//            @State private var change = false
+//            @State private var secondChange = false
+//            @State private var blendDuration = 1.0
+//
+//            var body: some View {
+//                VStack(spacing: 20) {
+//                    Circle()
+//                        .foregroundColor(.green)
+//                        .scaleEffect(change ? secondChange ? 0.1 : 0.3 : secondChange ? 0.5 : 1.0)
+//                        .animation(.spring(response: 1, dampingFraction: 0.1, blendDuration: blendDuration), value: change)
+//                        .animation(.spring(response: 10, dampingFraction: 1, blendDuration: blendDuration), value: secondChange)
+//
+//                     HStack {
+//                        Image(systemName: "hare")
+//                        Slider(value: $blendDuration, in: 0...2)
+//                        Image(systemName: "tortoise")
+//                    }.foregroundColor(.green).padding()
+//
+//                    Text("\(self.blendDuration)")
+//
+//                    Button("Change") {
+//                        withAnimation{
+//                            self.change.toggle()}
+//                    }.font(.title)
+//
+//                    Button("SecondChange") {
+//                        withAnimation{
+//                            self.secondChange.toggle()}
+//                    }.font(.title)
+//                }
+//            }
+
+
 //                .animation(.spring())
 //                .animation(Animation.linear(duration: timing))
 
@@ -39,6 +120,40 @@ struct AnimationTimingBootcamp: View {
 //            RoundedRectangle(cornerRadius: 20)
 //                .frame(width: isAnimating ? 350: 50, height: 100)
 //                .animation(Animation.easeOut(duration: timing))
+        }
+    }
+}
+
+struct Spring_BlendDuration: View {
+    @State private var change = false
+    @State private var secondChange = false
+    @State private var blendDuration = 1.0
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Circle()
+                .foregroundColor(.green)
+                .scaleEffect(change ? secondChange ? 0.1 : 0.3 : secondChange ? 0.5 : 1.0)
+                .animation(.spring(response: 1, dampingFraction: 0.1, blendDuration: blendDuration), value: change)
+                .animation(.spring(response: 10, dampingFraction: 1, blendDuration: blendDuration), value: secondChange)
+
+             HStack {
+                Image(systemName: "hare")
+                Slider(value: $blendDuration, in: 0...2)
+                Image(systemName: "tortoise")
+            }.foregroundColor(.green).padding()
+
+            Text("\(self.blendDuration)")
+
+            Button("Change") {
+                withAnimation{
+                    self.change.toggle()}
+            }.font(.title)
+
+            Button("SecondChange") {
+                withAnimation{
+                    self.secondChange.toggle()}
+            }.font(.title)
         }
     }
 }
