d0a8793f168235212cfbbe34852ec2910d328684
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-05-17 22:47:00 +0900

    feat: User Experience and Animations in SwiftUI app | Todo List #5

---
+++ SwiftUIThinking/Views/NoItemView.swift
@@ -0,0 +1,75 @@
+//
+//  NoItemView.swift
+//  SwiftUIThinking
+//
+//  Created by Eddy on 2023/05/17.
+//
+
+import SwiftUI
+
+struct NoItemView: View {
+
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("Add something")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : .accentColor)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : .accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0,
+                    y: animate ? 50 : 30)
+                // 크기 효과
+                .scaleEffect(animate ? 1.1 : 1.0)
+                // 위치 조정
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+
+    func addAnimation() {
+        // 2번 호출되는 것을 막기 위해 만들어둔다
+        guard !animate else { return }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct NoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            NoItemView()
+                .navigationTitle("Title")
+        }
+    }
+}
