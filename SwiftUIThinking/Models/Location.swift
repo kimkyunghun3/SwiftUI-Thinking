17990626d2d30e630b028cbae277adae0261dcda
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-05-24 20:40:03 +0900

    feat: Project Setup with Assets and Custom Data Model | SwiftUI Map App #1

---
+++ SwiftUIThinking/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftUIThinking
+//
+//  Created by Eddy on 2023/05/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
