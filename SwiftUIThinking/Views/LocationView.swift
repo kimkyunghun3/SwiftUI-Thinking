1c77bb0aad7d745883e47cc853931cbb08de952e
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-06-06 23:59:45 +0900

    feat: Custom Map Annotation Pins for SwiftUI MapKit Map | SwiftUI Map App #6

--- SwiftUIThinking/Views/LocationView.swift
+++ SwiftUIThinking/Views/LocationView.swift
@@ -14,27 +14,14 @@ struct LocationView: View {
 
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
 
             VStack(spacing: 0) {
                 header
                     .padding()
-
                 Spacer()
-
-                ZStack {
-                    ForEach(vm.location) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(
-                                    insertion: .move(edge: .trailing),
-                                    removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationPreviewStack
             }
         }
     }
@@ -74,5 +61,35 @@ extension LocationView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.location,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+
+    private var locationPreviewStack: some View {
+        ZStack {
+            ForEach(vm.location) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
