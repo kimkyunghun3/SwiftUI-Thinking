a1082faaee57317a6d95db098ed9d65b6c8fd7f7
kimkyunghun3 <kh.kim@lifesemantics.kr>
2023-06-07 20:08:08 +0900

    refactor: MapApp 폴더구조 하나로 변경

---
+++ SwiftUIThinking/LongPressGestureBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftUIThinking
+//
+//  Created by Eddy on 2023/06/07.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LongPressGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGestureBootcamp()
+    }
+}
